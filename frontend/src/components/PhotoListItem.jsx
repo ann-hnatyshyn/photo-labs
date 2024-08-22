2871aad901387c236fed54f251e8cbfbbf558d04
Ann <a.m.hnatyshyn@gmail.com>
2024-08-22 09:59:20 -0300

    fixed like button

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,23 +3,9 @@ import App from "../App.jsx";
 import PhotoList from "./PhotoList.jsx";
 import PhotoFavButton from "./PhotoFavButton.jsx";
 import "../styles/PhotoListItem.scss";
-import FavIcon from "./FavIcon.jsx";
 
 
 const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
-
-// const photos = [...Array(3)];
-// const sampleArrayOfPhotos = photos.map((photo, i) => 
-
-//   <PhotoListItem 
-//     key={i} 
-//     id={sampleDataForPhotoListItem.id}
-//     location={sampleDataForPhotoListItem.location}
-//     imageSource={sampleDataForPhotoListItem.imageSource}
-//     username={sampleDataForPhotoListItem.username}
-//     profile={sampleDataForPhotoListItem.profile}
-//   />
-//   )
 
   return (
     <div className="photo-list__item">
