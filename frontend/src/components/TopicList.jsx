7150f805d22689a0a5fd56a0b3be5f079018ae23
Ann <a.m.hnatyshyn@gmail.com>
2024-08-23 10:29:39 -0300

    add mock data to components

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,29 +1,13 @@
 import React from "react";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
+import topics from "../mocks/topics"
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
 const TopicList = (topic) => {
   return (
     <div className="topic-list__item">
-      {sampleDataForTopicList.map((topic) => (
+      {topics.map((topic) => (
         <TopicListItem
         key={topic.id}
         id={topic.id}
