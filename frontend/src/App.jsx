7646b18cd61fd40232e7bdba226fd4b761a862d0
Ann <a.m.hnatyshyn@gmail.com>
2024-08-23 10:24:33 -0300

    delete HomeRoute Component, edit HomeRoute file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.scss';
-import PhotoList from '../src/components/PhotoList';
-import TopNavigation from 'components/TopNavigationBar';
 import HomeRoute from 'routes/HomeRoute';
 
 
